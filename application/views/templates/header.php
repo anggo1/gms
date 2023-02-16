a102323eeee8cc46bc46f53c9fa78f79fa69cd96
anggoro wibowo <anggowibowo@gmail.com>
2023-02-16 19:05:42 +0700

    update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,7 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome-5.5.0/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/ionicons-2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bbootstrap 4 -->
