434d325d3984ab57697ec45f694ca59a87f24280
anggoro wibowo <anggowibowo@gmail.com>
2023-03-08 15:57:40 +0700

    bast

--- application/models/body_repair_model/Mod_bast.php
+++ application/models/body_repair_model/Mod_bast.php
@@ -131,28 +131,96 @@ class Mod_bast extends CI_Model
             $lb_kn='0';
         }
 
-        $lb_kr=""; if (!empty($data['lb_kr'])){ $lb_kr=$data['lb_kr']; }else { $lb_kr='0'; }
-        $ban_serep=""; if (!empty($data['ban_serep'])){ $ban_serep=$data['ban_serep']; }else { $ban_serep='0'; }
-        $stnk=""; if (!empty($data['stnk'])){ $stnk=$data['stnk']; }else { $stnk='0'; }
-        $sign_kn=""; if (!empty($data['sign_kn'])){ $sign_kn=$data['sign_kn']; }else { $sign_kn='0'; }
-        $sign_kr=""; if (!empty($data['sign_kr'])){ $sign_kr=$data['sign_kr']; }else { $sign_kr='0'; }
-        $kc_roda=""; if (!empty($data['kc_roda'])){ $kc_roda=$data['kc_roda']; }else { $kc_roda='0'; }
-        $kps=""; if (!empty($data['kps'])){ $kps=$data['kps']; }else { $kps='0'; }
-        $lmp_rem_kn=""; if (!empty($data['lmp_rem_kn'])){ $lmp_rem_kn=$data['lmp_rem_kn']; }else { $lmp_rem_kn='0'; }
-        $lmp_rem_kr=""; if (!empty($data['lmp_rem_kr'])){ $lmp_rem_kr=$data['lmp_rem_kr']; }else { $lmp_rem_kr='0'; }
-        $dongkrak=""; if (!empty($data['dongkrak'])){ $dongkrak=$data['dongkrak']; }else { $dongkrak='0'; }
-        $keur=""; if (!empty($data['keur'])){ $keur=$data['keur']; }else { $keur='0'; }
-        $lmp_kota_kn=""; if (!empty($data['lmp_kota_kn'])){ $lmp_kota_kn=$data['lmp_kota_kn']; }else { $lmp_kota_kn='0'; }
-        $lmp_kota_kr=""; if (!empty($data['lmp_kota_kr'])){ $lmp_kota_kr=$data['lmp_kota_kr']; }else { $lmp_kota_kr='0'; }
-        $video=""; if (!empty($data['video'])){ $video=$data['video']; }else { $video='0'; }
-        $buku_jr=""; if (!empty($data['buku_jr'])){ $buku_jr=$data['buku_jr']; }else { $buku_jr='0'; }
-        $spion_kn=""; if (!empty($data['spion_kn'])){ $spion_kn=$data['spion_kn']; }else { $spion_kn='0'; }
-        $spion_kr=""; if (!empty($data['spion_kr'])){ $spion_kr=$data['spion_kr']; }else { $spion_kr='0'; }
-        $tv=""; if (!empty($data['tv'])){ $tv=$data['tv']; }else { $tv='0'; }
-        $siu=""; if (!empty($data['siu'])){ $siu=$data['siu']; }else { $siu='0'; }
-        $tape=""; if (!empty($data['tape'])){ $tape=$data['tape']; }else { $tape='0'; }
-        $kc_kontak=""; if (!empty($data['kc_kontak'])){ $kc_kontak=$data['kc_kontak']; }else { $kc_kontak='0'; }
-
+        $ket1=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket2=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket3=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket4=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket5=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket6=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket7=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket8=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket9=""; if (!empty($data['ket1'])){ $ket1=$data['ket1']; }else { $ket1='0'; }
+        $ket10=""; if (!empty($data['ket10'])){ $ket10=$data['ket10']; }else { $ket10='0'; }
+        $ket11=""; if (!empty($data['ket11'])){ $ket11=$data['ket11']; }else { $ket11='0'; }
+        $ket12=""; if (!empty($data['ket12'])){ $ket12=$data['ket12']; }else { $ket12='0'; }
+        $ket13=""; if (!empty($data['ket13'])){ $ket13=$data['ket13']; }else { $ket13='0'; }
+        $ket14=""; if (!empty($data['ket14'])){ $ket14=$data['ket14']; }else { $ket14='0'; }
+        $ket15=""; if (!empty($data['ket15'])){ $ket15=$data['ket15']; }else { $ket15='0'; }
+        $ket16=""; if (!empty($data['ket16'])){ $ket16=$data['ket16']; }else { $ket16='0'; }
+        $ket17=""; if (!empty($data['ket17'])){ $ket17=$data['ket17']; }else { $ket17='0'; }
+        $ket18=""; if (!empty($data['ket18'])){ $ket18=$data['ket18']; }else { $ket18='0'; }
+        $ket19=""; if (!empty($data['ket19'])){ $ket19=$data['ket19']; }else { $ket19='0'; }
+        $ket20=""; if (!empty($data['ket20'])){ $ket20=$data['ket20']; }else { $ket20='0'; }
+        $ket21=""; if (!empty($data['ket21'])){ $ket21=$data['ket21']; }else { $ket21='0'; }
+        $ket22=""; if (!empty($data['ket22'])){ $ket22=$data['ket22']; }else { $ket22='0'; }
+        $ket23=""; if (!empty($data['ket23'])){ $ket23=$data['ket23']; }else { $ket23='0'; }
+        $ket24=""; if (!empty($data['ket24'])){ $ket24=$data['ket24']; }else { $ket24='0'; }
+        $ket25=""; if (!empty($data['ket25'])){ $ket25=$data['ket25']; }else { $ket25='0'; }
+        $ket26=""; if (!empty($data['ket26'])){ $ket26=$data['ket26']; }else { $ket26='0'; }
+        $ket27=""; if (!empty($data['ket27'])){ $ket27=$data['ket27']; }else { $ket27='0'; }
+        $ket28=""; if (!empty($data['ket28'])){ $ket28=$data['ket28']; }else { $ket28='0'; }
+        $ket29=""; if (!empty($data['ket29'])){ $ket29=$data['ket29']; }else { $ket29='0'; }
+        $ket30=""; if (!empty($data['ket30'])){ $ket30=$data['ket30']; }else { $ket30='0'; }
+        $ket31=""; if (!empty($data['ket31'])){ $ket31=$data['ket31']; }else { $ket31='0'; }
+        $ket32=""; if (!empty($data['ket32'])){ $ket32=$data['ket32']; }else { $ket32='0'; }
+        $ket33=""; if (!empty($data['ket33'])){ $ket33=$data['ket33']; }else { $ket33='0'; }
+        $ket34=""; if (!empty($data['ket34'])){ $ket34=$data['ket34']; }else { $ket34='0'; }
+        $ket35=""; if (!empty($data['ket35'])){ $ket35=$data['ket35']; }else { $ket35='0'; }
+        $ket36=""; if (!empty($data['ket36'])){ $ket36=$data['ket36']; }else { $ket36='0'; }
+        $ket37=""; if (!empty($data['ket37'])){ $ket37=$data['ket37']; }else { $ket37='0'; }
+        $ket38=""; if (!empty($data['ket38'])){ $ket38=$data['ket38']; }else { $ket38='0'; }
+        $ket39=""; if (!empty($data['ket39'])){ $ket39=$data['ket39']; }else { $ket39='0'; }
+        $ket40=""; if (!empty($data['ket40'])){ $ket40=$data['ket40']; }else { $ket40='0'; }
+        $ket41=""; if (!empty($data['ket41'])){ $ket41=$data['ket41']; }else { $ket41='0'; }
+        $ket42=""; if (!empty($data['ket42'])){ $ket42=$data['ket42']; }else { $ket42='0'; }
+        $ket43=""; if (!empty($data['ket43'])){ $ket43=$data['ket43']; }else { $ket43='0'; }
+        $ket44=""; if (!empty($data['ket44'])){ $ket44=$data['ket44']; }else { $ket44='0'; }
+        $ket45=""; if (!empty($data['ket45'])){ $ket45=$data['ket45']; }else { $ket45='0'; }
+        $ket46=""; if (!empty($data['ket46'])){ $ket46=$data['ket46']; }else { $ket46='0'; }
+        $ket47=""; if (!empty($data['ket47'])){ $ket47=$data['ket47']; }else { $ket47='0'; }
+        $ket48=""; if (!empty($data['ket48'])){ $ket48=$data['ket48']; }else { $ket48='0'; }
+        $ket49=""; if (!empty($data['ket48'])){ $ket49=$data['ket49']; }else { $ket49='0'; }
+        $ket50=""; if (!empty($data['ket50'])){ $ket50=$data['ket50']; }else { $ket50='0'; }
+        $ket51=""; if (!empty($data['ket51'])){ $ket51=$data['ket51']; }else { $ket51='0'; }
+        $ket52=""; if (!empty($data['ket52'])){ $ket52=$data['ket52']; }else { $ket52='0'; }
+        $ket53=""; if (!empty($data['ket53'])){ $ket53=$data['ket53']; }else { $ket53='0'; }
+        $ket54=""; if (!empty($data['ket54'])){ $ket54=$data['ket54']; }else { $ket54='0'; }
+        $ket55=""; if (!empty($data['ket55'])){ $ket55=$data['ket55']; }else { $ket55='0'; }
+        $ket56=""; if (!empty($data['ket56'])){ $ket56=$data['ket56']; }else { $ket56='0'; }
+        $ket57=""; if (!empty($data['ket57'])){ $ket57=$data['ket57']; }else { $ket57='0'; }
+        $ket58=""; if (!empty($data['ket58'])){ $ket58=$data['ket58']; }else { $ket58='0'; }
+        $ket59=""; if (!empty($data['ket59'])){ $ket59=$data['ket59']; }else { $ket59='0'; }
+        $ket60=""; if (!empty($data['ket60'])){ $ket60=$data['ket60']; }else { $ket60='0'; }
+        $ket61=""; if (!empty($data['ket61'])){ $ket61=$data['ket61']; }else { $ket61='0'; }
+        $ket62=""; if (!empty($data['ket62'])){ $ket62=$data['ket62']; }else { $ket62='0'; }
+        $ket63=""; if (!empty($data['ket63'])){ $ket63=$data['ket63']; }else { $ket63='0'; }
+        $ket64=""; if (!empty($data['ket64'])){ $ket64=$data['ket64']; }else { $ket64='0'; }
+        $ket65=""; if (!empty($data['ket65'])){ $ket65=$data['ket65']; }else { $ket65='0'; }
+        $ket66=""; if (!empty($data['ket66'])){ $ket66=$data['ket66']; }else { $ket66='0'; }
+        $ket67=""; if (!empty($data['ket67'])){ $ket67=$data['ket67']; }else { $ket67='0'; }
+        $ket68=""; if (!empty($data['ket68'])){ $ket68=$data['ket68']; }else { $ket68='0'; }
+        $ket69=""; if (!empty($data['ket69'])){ $ket69=$data['ket69']; }else { $ket69='0'; }
+        $ket70=""; if (!empty($data['ket70'])){ $ket70=$data['ket70']; }else { $ket70='0'; }
+        $ket71=""; if (!empty($data['ket71'])){ $ket71=$data['ket71']; }else { $ket71='0'; }
+        $ket72=""; if (!empty($data['ket72'])){ $ket72=$data['ket72']; }else { $ket72='0'; }
+        $ket73=""; if (!empty($data['ket73'])){ $ket73=$data['ket73']; }else { $ket73='0'; }
+        $ket74=""; if (!empty($data['ket74'])){ $ket74=$data['ket74']; }else { $ket74='0'; }
+        $ket75=""; if (!empty($data['ket75'])){ $ket75=$data['ket75']; }else { $ket75='0'; }
+        $ket76=""; if (!empty($data['ket76'])){ $ket76=$data['ket76']; }else { $ket76='0'; }
+        $ket77=""; if (!empty($data['ket77'])){ $ket77=$data['ket77']; }else { $ket77='0'; }
+        $ket78=""; if (!empty($data['ket78'])){ $ket78=$data['ket78']; }else { $ket78='0'; }
+        $ket79=""; if (!empty($data['ket79'])){ $ket79=$data['ket79']; }else { $ket79='0'; }
+        $ket80=""; if (!empty($data['ket80'])){ $ket80=$data['ket80']; }else { $ket80='0'; }
+        $ket81=""; if (!empty($data['ket81'])){ $ket81=$data['ket81']; }else { $ket81='0'; }
+        $ket82=""; if (!empty($data['ket82'])){ $ket82=$data['ket82']; }else { $ket82='0'; }
+        $ket83=""; if (!empty($data['ket83'])){ $ket83=$data['ket83']; }else { $ket83='0'; }
+        $ket84=""; if (!empty($data['ket84'])){ $ket84=$data['ket84']; }else { $ket84='0'; }
+        $ket85=""; if (!empty($data['ket85'])){ $ket85=$data['ket85']; }else { $ket85='0'; }
+        $ket86=""; if (!empty($data['ket86'])){ $ket86=$data['ket86']; }else { $ket86='0'; }
+        $ket87=""; if (!empty($data['ket87'])){ $ket87=$data['ket87']; }else { $ket87='0'; }
+        $ket88=""; if (!empty($data['ket88'])){ $ket88=$data['ket88']; }else { $ket88='0'; }
+        $ket89=""; if (!empty($data['ket89'])){ $ket89=$data['ket89']; }else { $ket89='0'; }
+        
         $sql = "INSERT INTO tbl_br_bast SET
             id_bast         ='".$kodeBaru."',
             tgl_bast        ='".$tgl_bast."',
@@ -161,37 +229,102 @@ class Mod_bast extends CI_Model
             no_pol          ='".$data['no_pol']."',
             nip_sp          ='".$data['nip_sp']."',
             nama_sp         ='".$data['nama_sp']."',
-            kaca_depan      ='".$data['kaca_depan']."',
-            kaca_belakang   ='".$data['kaca_belakang']."',
             spion           ='".$data['spion']."',
             solar           ='".$data['solar']."',
             seat            ='".$data['seat']."',
             ac              ='".$data['ac']."',
-            lb_kn           ='$lb_kn',
-            lb_kr           ='$lb_kr',
-            ban_serep       ='$ban_serep',
-            stnk            ='$stnk',
-            sign_kn         ='$sign_kn',
-            sign_kr         ='$sign_kr',
-            kc_roda         ='$kc_roda',
-            kps             ='$kps',
-            lmp_rem_kn      ='$lmp_rem_kn',
-            lmp_rem_kr      ='$lmp_rem_kr',
-            dongkrak        ='$dongkrak',
-            keur            ='$keur',
-            lmp_kota_kn     ='$lmp_kota_kn',
-            lmp_kota_kr     ='$lmp_kota_kr',
-            video           ='$video',
-            buku_jr         ='$buku_jr',
-            spion_kn        ='$spion_kn',
-            spion_kr        ='$spion_kr',
-            tv              ='$tv',
-            siu             ='$siu',
-            tape            ='$tape',
-            kc_kontak       ='$kc_kontak',
             keterangan      ='".$data['keterangan']."',
             status_bus      ='".$data['status_bus']."',
-            user            ='".$data['user']."'";
+            user            ='".$data['user']."',
+            kaca_depan          ='$ket1',
+            kaca _belakang          ='$ket2',
+            kc_kanan          ='$ket3',
+            kc_kiri          ='$ket4',
+            sp_kanan          ='$ket5',
+            sp_kiri          ='$ket6',
+            sp_dalam          ='$ket7',
+            body_depan          ='$ket8',
+            bemper_depan          ='$ket9',
+            body_kiri          ='$ket10',
+            body_kanan          ='$ket11',
+            body_belakang          ='$ket12',
+            bemper_belakang          ='$ket13',
+            pintu_dp_lh          ='$ket14',
+            pintu_dp_rh          ='$ket15',
+            pintu_bl_lh          ='$ket16',
+            lp_dp_lh          ='$ket17',
+            lp_dp_rh          ='$ket18',
+            lp_stop_bl_lh          ='$ket19',
+            lp_stop_bl_rh          ='$ket20',
+            lp_sbl_lh          ='$ket21',
+            lp_sbl_rh          ='$ket22',
+            lp_sdp_lh          ='$ket23',
+            kaca_depan          ='$ket24',
+            kaca_depan          ='$ket25',
+            kaca_depan          ='$ket26',
+            kaca_depan          ='$ket27',
+            kaca_depan          ='$ket28',
+            kaca_depan          ='$ket29',
+            kaca_depan          ='$ket30',
+            kaca_depan          ='$ket31',
+            kaca_depan          ='$ket32',
+            kaca_depan          ='$ket33',
+            kaca_depan          ='$ket34',
+            kaca_depan          ='$ket35',
+            kaca_depan          ='$ket36',
+            kaca_depan          ='$ket37',
+            kaca_depan          ='$ket38',
+            kaca_depan          ='$ket39',
+            kaca_depan          ='$ket40',
+            kaca_depan          ='$ket41',
+            kaca_depan          ='$ket42',
+            kaca_depan          ='$ket43',
+            kaca_depan          ='$ket44',
+            kaca_depan          ='$ket45',
+            kaca_depan          ='$ket46',
+            kaca_depan          ='$ket47',
+            kaca_depan          ='$ket48',
+            kaca_depan          ='$ket49',
+            kaca_depan          ='$ket50',
+            kaca_depan          ='$ket51',
+            kaca_depan          ='$ket52',
+            kaca_depan          ='$ket53',
+            kaca_depan          ='$ket54',
+            kaca_depan          ='$ket55',
+            kaca_depan          ='$ket56',
+            kaca_depan          ='$ket57',
+            kaca_depan          ='$ket58',
+            kaca_depan          ='$ket59',
+            kaca_depan          ='$ket60',
+            kaca_depan          ='$ket61',
+            kaca_depan          ='$ket62',
+            kaca_depan          ='$ket63',
+            kaca_depan          ='$ket64',
+            kaca_depan          ='$ket65',
+            kaca_depan          ='$ket66',
+            kaca_depan          ='$ket67',
+            kaca_depan          ='$ket68',
+            kaca_depan          ='$ket69',
+            kaca_depan          ='$ket70',
+            kaca_depan          ='$ket71',
+            kaca_depan          ='$ket72',
+            kaca_depan          ='$ket73',
+            kaca_depan          ='$ket74',
+            kaca_depan          ='$ket75',
+            kaca_depan          ='$ket76',
+            kaca_depan          ='$ket77',
+            kaca_depan          ='$ket78',
+            kaca_depan          ='$ket79',
+            kaca_depan          ='$ket80',
+            kaca_depan          ='$ket81',
+            kaca_depan          ='$ket82',
+            kaca_depan          ='$ket83',
+            kaca_depan          ='$ket84',
+            kaca_depan          ='$ket85',
+            kaca_depan          ='$ket86',
+            kaca_depan          ='$ket87',
+            kaca_depan          ='$ket88',
+            kaca_depan          ='$ket89'";
 
             $this->db->query($sql);
     
